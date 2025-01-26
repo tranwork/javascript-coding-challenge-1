60a35f4b9f3391ccd4890e0a6028eec4101cc1fd
Jenny <work.trantran@gmail.com>
2025-01-25 19:47:38 -0500

    Task 2 - Product Details

---
+++ cc_1.js
@@ -0,0 +1,8 @@
+// Task 2: Product Details
+let productName = "Laptop";
+const productPrice = 999.99;
+var isAvailable = false;
+
+console.log("Product Name:", productName, "Type:", typeof productName);
+console.log("Product Price:", productPrice, "Type:", typeof productPrice);
+console.log("Is Available:", isAvailable, "Type:", typeof isAvailable);
